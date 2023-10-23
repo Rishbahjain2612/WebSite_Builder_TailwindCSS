f695a6401f632eedb5bdb8fb1af21310a68a6cf5
Rishbahjain2612 <rishabhjain.01226@gmail.com>
2023-10-24 00:08:27 +0530

    Limited maximum number of elements that can be added to the canvas

--- JAVASCRIPT Components/positioning.js
+++ JAVASCRIPT Components/positioning.js
@@ -1,3 +1,4 @@
+// Get the Add buttons
 const addElements = document.querySelectorAll('.add');
 let count = parseInt(localStorage.getItem('count')) || 0;
 
@@ -12,12 +13,12 @@ for (let i = 0; i < count; i++)
             `<div
             class="p-2 border border-gray-400 border-alternatives-border text-sm font-semibold focus:outline-none flex justify-between" value="${elID}">
             <button class="focus:outline-none remove" onClick="removeEle(event)">
-                <i class="material-icons">remove</i>
+                <i class="material-icons" data-parent=${storedValue}>remove</i>
             </button>
             <div class="text-center w-full">
                 ${storedValue}
             </div>
-    </div>`;
+        </div>`;
 
         document.getElementById("selected-order").innerHTML += element;
     }
@@ -27,37 +28,60 @@ for (let i = 0; i < count; i++)
 addElements.forEach(add => {
     add.addEventListener('click', () => {
         let edit = "el_ordered" + count;
-        let element =
-            `<div
-        class="p-2 border border-gray-400 border-alternatives-border text-sm font-semibold focus:outline-none flex justify-between" value="${edit}">
-        <button class="focus:outline-none remove" onClick="removeEle(event)">
-            <i class="material-icons">remove</i>
-        </button>
-        <div class="text-center w-full">
-            ${add.value}
-        </div>
-    </div>`
+        let maxNum = add.getAttribute("data-maxnum");
 
-        localStorage.setItem(edit, add.value);
-        localStorage.setItem("count", count);
-        count++;
+        // Check if the element has already been added
+        if (maxNum === "0")
+        {
+            alert('Max number reached');
+        } else
+        {
+            maxNum--; // Decrement the max number
+            add.setAttribute("data-maxnum", maxNum); // Update the attribute
 
-        document.getElementById("selected-order").innerHTML += element;
-    })
+            let element =
+                `<div
+            class="p-2 border border-gray-400 border-alternatives-border text-sm font-semibold focus:outline-none flex justify-between" value="${edit}">
+            <button class="focus:outline-none remove" onClick="removeEle(event)">
+                <i class="material-icons" data-parent=${add.getAttribute("data-value")}>remove</i>
+            </button>
+            <div class="text-center w-full">
+                ${add.getAttribute("data-value")}
+            </div>
+        </div>`;
+
+            localStorage.setItem(edit, add.getAttribute("data-value"));
+            count++;
+            localStorage.setItem("count", count);
+
+            document.getElementById("selected-order").innerHTML += element;
+        }
+    });
 });
 
 function removeEle(event) {
-    let removeButton = event.target;
-    let parentElement = removeButton.parentNode.parentNode;
+    const removeButton = event.target;
+    console.log(removeButton);
+    console.log(removeButton.getAttribute("data-parent"));
+    const parentELE = document.getElementById(removeButton.getAttribute("data-parent"));
+    console.log(parentELE);
+    let maxnumParent = parentELE.getAttribute("data-maxnum");
+    console.log(maxnumParent);
+    maxnumParent++;
+    parentELE.setAttribute("data-maxnum", maxnumParent);
+
+    const parentElement = removeButton.parentElement.parentElement;
+    const value = parentElement.getAttribute('value');
+
+    // increment the max-count for the positioning element
 
     // Remove from localStorage
-    let value = parentElement.getAttribute('value');
     localStorage.removeItem(value);
 
     // Remove from DOM
-    parentElement.parentNode.removeChild(parentElement);
+    parentElement.remove();
 
     // Decrement count
     count--;
-    localStorage.setItem("count", count);
+    localStorage.setItem('count', count);
 }
